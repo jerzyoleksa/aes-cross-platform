aeae5da7fd27e867076540d7327df4fbd776998d
jerzyoleksa <jerzy.oleksa@gmail.com>
2022-12-21 20:20:09 +0100

    read encrypted message from txt file

--- decoder.py
+++ decoder.py
@@ -2,10 +2,18 @@ from Crypto.Cipher import AES
 from pkcs7 import PKCS7Encoder
 import pkcs7,threading, base64
 
-key = 'secret#456!23key'
+key = 'administrational'
 keyEnc = key.encode("utf8")
 
-enc_cipher = 'lbkY4V41z2IDY+zvEco69Q=='.encode("utf8");
+#1 message hardcoded
+#enc_cipher = 'lbkY4V41z2IDY+zvEco69Q=='.encode("utf8")
+
+#2 message from file
+with open('encrypted.txt', 'r') as file:
+    data = file.read()
+enc_cipher = data.encode("utf8")
+#end of file read
+
 keyEnc = key.encode("utf8")
 
 decodetext =  base64.b64decode(enc_cipher)
